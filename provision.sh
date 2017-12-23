3f5f6fb6f9401248d11f1ad35c37fe9d4e384e17
yanasem <yanase@yanasemasanorinoMacBook-Pro.local>
2017-12-23 14:05:31 +0900

    Added yarn and vim.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 echo "Installing nodejs and setting it up..."
 apt-get update >/dev/null 2>&1
-apt-get install -y nodejs npm git >/dev/null 2>&1
+apt-get install -y \
+ git \
+ nodejs \
+ npm \
+ yarn \
+ vim >/dev/null 2>&1
 #rm -rf /var/www
 #ln -fs /vagrant /var/www
