046c12282a1cd0ee03cfd0c265aa0b624f121954
yanasem <yanase@yanasemasanorinoMacBook-Pro.local>
2017-12-23 16:50:17 +0900

    Added symlink for react

--- provision.sh
+++ provision.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 echo "Installing nodejs and setting it up..."
 apt-get update >/dev/null 2>&1
-apt-get install -y \
- git \
- nodejs \
- npm \
- yarn \
- vim >/dev/null 2>&1
-#rm -rf /var/www
-#ln -fs /vagrant /var/www
+apt-get install -y git nodejs npm vim >/dev/null 2>&1
+
 echo "Setting up development environment..."
 ln -s /vagrant/react /home/vagrant
