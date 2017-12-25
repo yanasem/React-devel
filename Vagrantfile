a13539bb15eeb1218c75914b39c8fa6c5fcab8c2
yanasem <yanasenator@gmail.com>
2017-12-25 18:07:45 +0900

    Changed precise64 to trusty64,'

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,6 @@
 Vagrant::Config.run do |config|
- config.vm.box = "precise64"
+# config.vm.box = "precise64"
+ config.vm.box = "ubuntu/trusty64"
  config.vm.forward_port 80, 8080
  config.vm.provision "shell", path: "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 echo "Installing nodejs and setting it up..."
 apt-get update >/dev/null 2>&1
-apt-get install -y git nodejs npm vim >/dev/null 2>&1
+apt-get install -y git vim curl >/dev/null 2>&1
+#
+# nodejs
+# https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+#
+#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs >/dev/null 2>&1
+
+echo "Installing javascript stuffs and setting it up..."
+npm install yarn -g
+
+# apt-get install -y nodejs npm >/dev/null 2>&1
+# apt-get install -y yarn nodejs npm >/dev/null 2>&1
 
 echo "Setting up development environment..."
-ln -s /vagrant/react /home/vagrant
+if [ ! -e "/home/vagrant/react" ]; then
+  ln -s /vagrant/react /home/vagrant
+fi
