16b9052f86eab8b939d443ae010f687bb98f40d2
yanasem <yanasenator@gmail.com>
2017-12-25 18:34:47 +0900

    Added build-essential

--- provision.sh
+++ provision.sh
@@ -2,6 +2,7 @@
 echo "Installing nodejs and setting it up..."
 apt-get update >/dev/null 2>&1
 apt-get install -y git vim curl >/dev/null 2>&1
+apt-get install build-essential >/dev/null 2>&1
 #
 # nodejs
 # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
@@ -11,7 +12,8 @@ curl -sL https://deb.nodesource.com/setup_8.x | bash -
 apt-get install -y nodejs >/dev/null 2>&1
 
 echo "Installing javascript stuffs and setting it up..."
-npm install yarn -g
+# yarn
+curl -o- -L https://yarnpkg.com/install.sh | bash
 
 # apt-get install -y nodejs npm >/dev/null 2>&1
 # apt-get install -y yarn nodejs npm >/dev/null 2>&1
